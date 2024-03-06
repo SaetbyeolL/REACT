278f424bdabfebe008463cae9238e1336a7b7b7b
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-06 18:59:27 -0500

    useEffect

--- react-app/src/Button.js
+++ react-app/src/Button.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import styles from "./Button.module.css" //create-react-app converts css into javascript object(styles)
+import styles from "./Button.module.css"                 //create-react-app converts css into javascript object(styles)
 
 function Button({ text }) {
   return <button className={styles.btn}>{text}</button>; // create-react-app create random className
