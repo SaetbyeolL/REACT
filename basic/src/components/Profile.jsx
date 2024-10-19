109f22806f0f748e36068f416dcb064d9fc58bc1
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-10-18 23:24:46 -0400

    Creating avatar component

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
+import React from "react";
+import Avatar from "./Avatar";
 
-export default function Profile({image, name, title}) {
-    return (
-    <div className='profile'>
-        <img 
-            className='photo'
-            src={image}
-            alt='avatar'
-        />
-        <h1>{name}</h1>
-        <p>{title}</p>   
+export default function Profile({ image, name, title, isNew }) {
+  return (
+    <div className="profile">
+      <Avatar image={image} isNew={isNew} />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
-    )
-};
+  );
+}
